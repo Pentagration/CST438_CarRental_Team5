3d2b126abded27cbd5833e85cedca492468c0591
gonzalezcode <marcus68@protonmail.com>
2020-07-20 17:11:46 -0700

    Added project structure

---
+++ src/main/java/carRental/carRental/CarRental.java
@@ -0,0 +1,13 @@
+package carRental.carRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRental {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRental.class, args);
+    }
+
+}
