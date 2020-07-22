91c0cba514a02cbfe33b76dfa6190e1e63fc056d
jpettit <jpettit@csumb.edu>
2020-07-21 17:20:14 -0700

    CarReservation Progressing

--- src/main/java/carRental/domain/CarReservation.java
+++ src/main/java/carRental/domain/CarReservation.java
@@ -2,33 +2,39 @@ package carRental.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name="reservation")
+//@Table(name="reservation")
 public class CarReservation {
 
   @Id
   private long customerID;
   private long carID;
-  private String pickupDate;
-  private String returnDate;
+  private String fName;
+  private String lName;
+  private String email;
   private String pickupLocation;
+  private String pickupDate;
   private String returnLocation;
+  private String returnDate;
+  private String type;
 
-  public CarReservation(){
-    this(0, 0, "pickupDate", "returnDate", "pickupLocation", "returnLocation");
-  }
+  public CarReservation(){}
 
-  public CarReservation(long customerID, long carID, String pickupDate, String returnDate,
-      String pickupLocation, String returnLocation){
+  public CarReservation(long customerID, long carID, String fName, String lName, String email,
+      String pickupLocation, String pickupDate,
+      String returnLocation, String returnDate, String type){
     super();
     this.customerID = customerID;
     this.carID = carID;
-    this.pickupDate = pickupDate;
-    this.returnDate = returnDate;
+    this.fName = fName;
+    this.lName = lName;
+    this.email = email;
     this.pickupLocation = pickupLocation;
+    this.pickupDate = pickupDate;
     this.returnLocation = returnLocation;
+    this.returnDate = returnDate;
+    this.type = type;
   }
 
   public long getCustomerID() {
@@ -47,20 +53,28 @@ public class CarReservation {
     this.carID = carID;
   }
 
-  public String getPickupDate() {
-    return pickupDate;
+  public String getfName() {
+    return fName;
   }
 
-  public void setPickupDate(String pickupDate) {
-    this.pickupDate = pickupDate;
+  public void setfName(String fName) {
+    this.fName = fName;
   }
 
-  public String getReturnDate() {
-    return returnDate;
+  public String getlName() {
+    return lName;
   }
 
-  public void setReturnDate(String returnDate) {
-    this.returnDate = returnDate;
+  public void setlName(String lName) {
+    this.lName = lName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
   }
 
   public String getPickupLocation() {
@@ -71,6 +85,14 @@ public class CarReservation {
     this.pickupLocation = pickupLocation;
   }
 
+  public String getPickupDate() {
+    return pickupDate;
+  }
+
+  public void setPickupDate(String pickupDate) {
+    this.pickupDate = pickupDate;
+  }
+
   public String getReturnLocation() {
     return returnLocation;
   }
@@ -79,10 +101,28 @@ public class CarReservation {
     this.returnLocation = returnLocation;
   }
 
+  public String getReturnDate() {
+    return returnDate;
+  }
+
+  public void setReturnDate(String returnDate) {
+    this.returnDate = returnDate;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
   @Override
   public String toString() {
     return "CarReservation [customerId=" + customerID + ", carID=" + carID +
-        ", pickupDate=" + pickupDate + ", returnDate=" + returnDate +
-        ", pickupLocation=" + pickupLocation + ", returnLocation=" + returnLocation + "]";
+        ", fName=" + fName + ", lName=" + lName + ", email=" + email +
+        ", pickupLocation=" + pickupLocation + ", pickupDate=" + pickupDate +
+        ", returnLocation=" + returnLocation + ", returnDate=" + returnDate +
+        ", type=" + type + "]";
   }
 }
