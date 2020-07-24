6086cf4f457dfaf19398a4ffa90eff261bb02760
Adam Houser <aphouser@gmail.com>
2020-07-23 18:24:56 -0700

    A little work on RestController, Service, and Reporitory

--- src/main/java/carRental/controller/CarReservationRestController.java
+++ src/main/java/carRental/controller/CarReservationRestController.java
@@ -16,8 +16,23 @@ public class CarReservationRestController {
 
   // this is a get method for finding all reservations
   @GetMapping("/api/reservations")
-  public ResponseEntity<CarReservation> getCarReservation(@PathVariable("city") String cityName) {
-    CarReservation carReservation = carReservationService.getResInfo(cityName);
+  public ResponseEntity<Iterable<CarReservation>> getAllCarReservation() {
+    Iterable<CarReservation> carReservation = carReservationService.getResInfo();
+
+    // look up city info from database.  Might be multiple cities with same name.
+    if (carReservation == null) {
+      // reservation not found.  Send 404 return code.
+      return new ResponseEntity<Iterable<CarReservation>>( HttpStatus.NOT_FOUND);
+    } else {
+      // return 200 status code (OK) and city information in JSON format
+      return new ResponseEntity<Iterable<CarReservation>>(carReservation, HttpStatus.OK);
+    }
+  }
+
+  // this is a get method for finding a single reservation
+  @GetMapping("/api/reservations/{id}")
+  public ResponseEntity<CarReservation> getSingleCarReservation(@PathVariable("id") int resID) {
+    CarReservation carReservation = carReservationService.getResInfo(resID);
 
     // look up city info from database.  Might be multiple cities with same name.
     if (carReservation == null) {
@@ -29,10 +44,19 @@ public class CarReservationRestController {
     }
   }
 
-  // this is a get method for finding a single reservation
-  @GetMapping("/api/reservations/{id}")
-
   // this is a get method for creating a new reservation
   @PostMapping("/api/reservations/new")
+  public ResponseEntity<CarReservation> getNewCarReservation(@PathVariable("city") String cityName) {
+    CarReservation carReservation = carReservationService.getResInfo(cityName);
+
+    // look up city info from database.  Might be multiple cities with same name.
+    if (carReservation == null) {
+      // reservation not found.  Send 404 return code.
+      return new ResponseEntity<CarReservation>( HttpStatus.NOT_FOUND);
+    } else {
+      // return 200 status code (OK) and city information in JSON format
+      return new ResponseEntity<CarReservation>(carReservation, HttpStatus.OK);
+    }
+  }
 
 }
--- src/main/java/carRental/domain/CarReservationRepository.java
+++ src/main/java/carRental/domain/CarReservationRepository.java
@@ -3,5 +3,6 @@ package carRental.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CarReservationRepository extends CrudRepository<CarReservation, Long> {
-
+  CarReservation findByID(int resID);
+  Iterable<CarReservation> findAll();
 }
