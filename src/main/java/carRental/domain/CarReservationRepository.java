92bf124ce903de20bab5a7ea75356cbc9f90ec69
Colinr1994 <colinr1994@gmail.com>
2020-07-23 21:17:31 -0700

    Search by email now workinggit status!

--- src/main/java/carRental/domain/CarReservationRepository.java
+++ src/main/java/carRental/domain/CarReservationRepository.java
@@ -8,7 +8,10 @@ public interface CarReservationRepository extends CrudRepository<CarReservation,
 //  CarReservation findByID(int resID);
 //  Iterable<CarReservation> findAll();
 	
-	@Query("Select u from reservation u WHERE u.email =?1")
+	//@Query(value = "SELECT * FROM Users u WHERE u.status = ?1", nativeQuery = true)
+	//User findUserByStatusNative(Integer status);
+	
+	@Query(value= "Select * from reservation u WHERE u.email =?1", nativeQuery = true)
 	List<CarReservation> findByEmail(String email);
 	
 	
